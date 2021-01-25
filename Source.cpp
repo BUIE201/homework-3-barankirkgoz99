ec2623fd58a903c48bdf56e254cc6fc536223ef0
baran kırkgöz <krkgzbaran@hotmail.com>
2021-01-25 23:19:20 +0300

    print tree and inserting functions

--- Source.cpp
+++ Source.cpp
@@ -14,6 +14,36 @@ struct Node
 	Node(int iIn) : i(iIn), pLeft(nullptr), pRight(nullptr) {}
 };
 
+void Inserting(Node*& pRoot, Node* pNewNode)
+{
+	if (!pRoot)
+		pRoot = pNewNode;
+	else
+	{
+		if (pNewNode->i < pRoot->i)
+			Inserting(pRoot->pLeft, pNewNode);
+		else
+			Inserting(pRoot->pRight, pNewNode);
+	}
+}
+
+
+void PrintTree(Node* pRoot, int Level)
+{
+	if (!pRoot)
+		return;
+
+	PrintTree(pRoot->pRight, Level + 1);
+
+	for (int i = 0; i < Level; i++)
+		cout << "  ";
+
+	cout << pRoot->i << endl;
+
+	PrintTree(pRoot->pLeft, Level + 1);
+}
+
+
 void main()
 {
 }

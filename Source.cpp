af6092a838734b9785d42d76495d8a4e6b125027
baran kırkgöz <krkgzbaran@hotmail.com>
2021-01-25 23:47:34 +0300

    DeleteNodeFromTree DeleteNodeWithTwoChildren

--- Source.cpp
+++ Source.cpp
@@ -43,6 +43,48 @@ void PrintTree(Node* pRoot, int Level)
 	PrintTree(pRoot->pLeft, Level + 1);
 }
 
+void DeleteNodeWithTwoChildren(Node*& q, Node*& p)
+{
+    if (p->pRight)
+    {
+        DeleteNodeWithTwoChildren(q, p->pRight);
+        return;
+    }
+
+    p->i = q->i;
+    q = p;
+    p = p->pLeft;
+}
+
+void DeleteNodeFromTree(Node*& pRoot, int i)
+{
+    if (!pRoot)
+        return;
+
+    if (pRoot->i < i)
+    {
+        DeleteNodeFromTree(pRoot->pRight, i);
+        return;
+    }
+
+    if (pRoot->i > i)
+    {
+        DeleteNodeFromTree(pRoot->pLeft, i);
+        return;
+    }
+
+    Node* q = pRoot;
+    if (!q->pRight)
+        pRoot = q->pLeft;
+    else if (!q->pLeft)
+        pRoot = q->pRight;
+    else
+        DeleteNodeWithTwoChildren(q, q->pLeft);
+
+    delete q;
+}
+
+
 
 void main()
 {

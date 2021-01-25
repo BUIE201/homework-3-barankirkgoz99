26742505ec9e8f245dcad9c7f2a6f7e388162cca
baran kırkgöz <krkgzbaran@hotmail.com>
2021-01-25 23:10:16 +0300

    node is created

--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+struct Node
+{
+	int i;
+	Node* pLeft;
+	Node* pRight;
+
+	Node(int iIn) : i(iIn), pLeft(nullptr), pRight(nullptr) {}
+};
 
 void main()
 {
